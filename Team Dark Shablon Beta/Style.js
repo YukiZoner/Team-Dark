9ebf45c319b6e938807090cb31f822d283e5ac85
YukiZoner <58343014+YukiZoner@users.noreply.github.com>
2022-02-09 20:36:46 +0200

    my site

---
+++ Team Dark Shablon Beta/Style.js
@@ -0,0 +1,11 @@
+window.onscroll = () => {
+    const header = document.querySelector('.navbar');
+    const Y = window.scrollY
+
+    if(Y > 350) {
+        header.classList.add("navbar_fixed");
+    }   else if (Y < 5) {
+        header.classList.remove("navbar_fixed");
+    }
+    
+}
