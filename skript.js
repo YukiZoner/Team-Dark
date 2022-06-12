c4c7e3f1e50d176d1643dca0cd3800b84dba0c49
YukiZoner <58343014+YukiZoner@users.noreply.github.com>
2022-06-12 22:22:21 +0300

    Add files via upload

---
+++ skript.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">var submitted=false;</script>
+<iframe name="hidden_iframe" id="hidden_iframe"
+style="display:none;" onload="if(submitted)
+{window.location='https://vk.com/landingpagefree';}"></iframe>
+<form action="111111111" method="post"
+target="hidden_iframe" onsubmit="submitted=true;">
